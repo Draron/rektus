f528c1308484f6aaa8cf15228d14defc0322e3ce
Draron <7995746+Draron@users.noreply.github.com>
2017-11-02 15:40:47 +0100

    health *10

--- person.py
+++ person.py
@@ -2,7 +2,7 @@ from random import *
 class Person():
 	def __init__(self, heightp0, heightp1, gen):
 		self.sex = randint(0, 1)
-		self.health = randint(0, 1000)
+		self.health = randint(0, 10000)
 		self.height = generate_height(heightp0, heightp1, self.sex) 
 		self.special = [gen, self.sex, self.height, self.health]
 def generate_height(heightp0, heightp1, sex):
