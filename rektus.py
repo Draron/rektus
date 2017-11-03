7d03fedf92649dd74bcb3a08a38e6f11232b514e
Draron <7995746+Draron@users.noreply.github.com>
2017-11-03 11:11:34 +0100

    changed most loops to for() if possible

--- rektus.py
+++ rektus.py
@@ -1,5 +1,6 @@
 from random import randint
 from person import *
+
 def exist():
     society = [Person(1780, 1670, 0)]
     i = 0
@@ -12,45 +13,47 @@ def exist():
     global ever
     ever += 50
     return society
+
 def average():
-    a = 0   #counter for average geneation
-    b = 0   #counter for female to male ratio
-    c = 0   #counter for average height
-    d = 0   #counter for average health
+    #a = 0   #counter for average geneation
+    #b = 0   #counter for female to male ratio
+    #c = 0   #counter for average height
+    #d = 0   #counter for average health
+    x = len(society)
     f = 0   #number of females (used to determine ftm ratio)
     m = 0   #number of males (used to determine ftm ratio)
-    x = len(society) 
     avgn = 0    #average gen temp value
     avht = 0    #average height temp value
     avhl = 0    #average health temp value
-    while a < x:
+    for a in range(0, x):
         avgn += society[a].special[0]
-        a += 1
+        #a += 1
     avgn = avgn / x
-    while b < x:
+    for b in range(0, x):
         if(society[b].special[1] == 1):
             m += 1
-            b += 1
+            #b += 1
         else:
             f += 1
-            b += 1
+            #b += 1
         g = f + m
         g = f / g
         fr = g * 100        #female ratio
         mr = (1 - g) * 100  #male ratio
-    while c < x:
+    for c in range(0, x):
         avht += society[c].special[2]
-        c += 1
+        #c += 1
     avht = avht / x
     
-    while d < x:
+    for d in range(0, x):
         avhl += society[d].special[3]
-        d += 1
+        #d += 1
     avhl = avhl / x
-    print("Average Gen: ", round(avgn), ", height:", round(avht), ", health: ", round(avhl), "f:m ratio: ", round(fr, 2), ":", round(mr, 2), ".", ever)
+    print("Average Gen: ", round(avgn), ", height:", round(avht), ", health: ", round(avhl), "f:m ratio: ", round(fr, 2), ":", round(mr, 2), ".", ever, f + m )
+
 def die():
     a = 0
-    lifetime = randint(1, 310)
+    lifetime = randint(1, 350)
     while(a < len(society)): 
             if((society[a].special[3] - lifetime) > 0):
                 society[a].special[3] -= lifetime
@@ -60,6 +63,7 @@ def die():
                 except ValueError:
                     print("problem removing", society[a].special, "@", society[a])
             a += 1
+
 def date():
     a = 0
     pairs = []
@@ -78,6 +82,7 @@ def date():
         pairs += [[x, y]]
         a += 2
     return(pairs)
+
 def reproduce(pairs):#, society):
     #society = society
     a = 0
@@ -91,6 +96,7 @@ def reproduce(pairs):#, society):
             pass
         a+=1
     return(society)
+
 ever = 0
 society = exist()
 average()   
